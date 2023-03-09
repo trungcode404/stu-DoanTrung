fafe09a44f6f09045be7db7c5d4c4bad72258bfc
huutrungdoan2202@gmail.com <huutrungdoan2202@gmail.com>
2023-03-09 20:43:33 +0700

    Practices

---
+++ react-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/330036522_1417751475666407_8508087773852649230_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=0debeb&_nc_ohc=57lOGItD1CUAX_6wrxj&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfB1uBCk5XGHghAsjeZKO0uc67f7Qu-Fg1R0Hs0WmL_APg&oe=640EB678" />
+        <p>
+          Hê sờ lô hê sờ ly ! mình tên là Nhím đầu bò
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/HoangLeThaoLy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           Link facebook đồ đó  
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
